90a225e8fd195b55ac6c19cee66e04bb16a615d9
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2020-01-07 23:48:25 -0200

    Create the mvp with no styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import api from './services/api';
+import { mountTweetUrl, colors } from './services/utils';
 
 function App() {
+  const [quote, setQuote] = useState({
+    text: '',
+    author: '',
+  });
+
+  const [color, setColor] = useState('#000');
+
+  const fetchData = useCallback(async () => {
+    const response = await api.get('/random');
+    const data = response.data;
+    if (data.content.length > 280) {
+      fetchData();
+    } else {
+      setQuote({ text: data.content, author: data.author });
+      changeColor();
+    }
+  }, []);
+
+  const changeColor = () => {
+    const index = Math.floor(Math.random() * colors.length);
+    setColor(colors[index]);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const handleClick = e => {
+    fetchData();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' id='quote-box' style={{ backgroundColor: color }}>
+      <h1 id='text'>{quote.text}</h1>
+      <h3 id='author'>{quote.author}</h3>
+      <button id='new-quote' type='submit' onClick={handleClick}>
+        New quote
+      </button>
+      <a href={mountTweetUrl(quote)} id='tweet-quote'>
+        Tweet
+      </a>
     </div>
   );
 }
---
+++ src/services/utils.js
@@ -0,0 +1,18 @@
+export function mountTweetUrl(quote) {
+  let { text, author } = quote;
+  text = text.split(' ').join('%20');
+  author = author.split(' ').join('%20');
+  const url = `https://twitter.com/intent/tweet?hashtags=quotes&text=%22${text}%22%20-%20${author}`;
+  return url;
+}
+
+export const colors = [
+  '#222',
+  '#7159C1',
+  '#2292A4',
+  '#F49CBB',
+  '#008148',
+  '#00BD9D',
+  '#DA2C38',
+  '#43291F',
+];
