231769a9419dbcaff909a7275a62a6bb7279f1f2
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2020-01-09 00:05:45 -0200

    Updates

---
+++ precache-manifest.03f1f3ed51cfcaaf51668aa27ad7d759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af36a9ed2b6e90f4c29fff558305bb0d",
+    "url": "/fcc-random-quote-machine/index.html"
+  },
+  {
+    "revision": "c6305e48f0a5cebaf746",
+    "url": "/fcc-random-quote-machine/static/css/main.1d6dd537.chunk.css"
+  },
+  {
+    "revision": "c1699c48307e3174e283",
+    "url": "/fcc-random-quote-machine/static/js/2.646e6c02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fcc-random-quote-machine/static/js/2.646e6c02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6305e48f0a5cebaf746",
+    "url": "/fcc-random-quote-machine/static/js/main.41f17736.chunk.js"
+  },
+  {
+    "revision": "27cd5634bd2e68ef9f9a",
+    "url": "/fcc-random-quote-machine/static/js/runtime-main.3efeb219.js"
+  }
+]);
